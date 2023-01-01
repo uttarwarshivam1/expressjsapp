7c3af58d8d94c46b9866e842ac35d63ca3c56667
uttarwarshivam1 <uttarwarshivam1@gmail.com>
2023-01-02 00:37:54 +0530

    rtyui

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express=require("express")
 const app=express()
+const path=require("path")
 app.get("/",(req,res)=>{
     res.send("hei i am alive")
 })
+app.get("/home",function(req,resp){
+
+    resp.sendFile(path.join(__dirname,"./public/index.html"));
+})
 app.listen(3000,()=>{
-    console.log('listening the port 8000')
+    console.log('listening the port 3000');
 })
