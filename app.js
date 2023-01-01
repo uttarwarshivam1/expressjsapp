9f2adf1494f356bea5fe0c6af935680a984df6d0
uttarwarshivam1 <uttarwarshivam1@gmail.com>
2023-01-02 00:54:16 +0530

    ioio

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const path=require("path")
 app.get("/",(req,res)=>{
     res.send("hei i am alive")
 })
-app.get("/home",(req,resp)=>{
+app.get("/home",(req,res)=>{
 
-    resp.sendFile(path.join(__dirname,"./public/index.html"));
+    res.sendFile(path.join(__dirname,"./public/index.html"));
 })
 app.listen(5000,()=>{
     console.log('listening the port 3000');
